679adb07cbc0bacfb1cd98dbd559c6672498533a
Iamsdt <shudiptotrafder@gmail.com>
2018-06-23 11:16:41 +0600

    grid view with color box
    Took 26 seconds

---
+++ lib/list/Grid.dart
@@ -0,0 +1,64 @@
+import 'package:flutter/material.dart';
+import 'package:playing_with_flutter/utils/Extension.dart';
+import 'dart:math';
+
+//todo post in blog
+
+class GridList extends StatelessWidget {
+  final items = List<String>.generate(10000, (i) => "Item $i");
+
+  @override
+  Widget build(BuildContext context) {
+    return new MaterialApp(
+      title: "Material App",
+      home: Scaffold(
+          appBar: getAppBar("Playing with GridView"),
+          floatingActionButton: getFab(context),
+          body: OrientationBuilder(
+            builder: (context, orientation) {
+              return GridView.builder(
+                  itemCount: items.length,
+                  gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                      crossAxisCount:
+                          orientation == Orientation.portrait ? 2 : 3),
+                  itemBuilder: (context, index) {
+                    return Container(
+                      color: getRandomColor(),
+                      child: ListTile(
+                        title: Text(items[index]),
+                        onTap: () {
+                          Scaffold
+                              .of(context)
+                              .showSnackBar(getSnackBar("position:$index"));
+                        },
+                      ),
+                    );
+                  });
+            },
+          )),
+    );
+  }
+
+  getRandomColor() {
+    var random = new Random();
+    var list = [
+      Colors.orange,
+      Colors.amber,
+      Colors.indigo,
+      Colors.blue,
+      Colors.green,
+      Colors.lime,
+      Colors.red,
+      Colors.brown,
+      Colors.deepOrange,
+      Colors.deepPurple,
+      Colors.pink,
+      Colors.purple,
+      Colors.yellow,
+    ];
+
+    var number = random.nextInt(list.length - 1);
+
+    return list[number];
+  }
+}
